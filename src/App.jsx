fb9736fb211be05d34b156dfb8469c54afe29ce2
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-12-27 21:02:09 +0530

    footer componant and header componant added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 
 import './App.css'
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import ListTodoComponent from './components/ListTodoComponent'
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   
   return (
     <>
-    <ListTodoComponent/>
+    <BrowserRouter> 
+    <HeaderComponent/>
+    <Routes>
+      {/* http://localhost:8082 */}
+      <Route path='/' element={<ListTodoComponent/>}></Route>
+      <Route path='/todos' element ={<ListTodoComponent/>} ></Route>
+
+    </Routes>
+    <FooterComponent/>
+    </BrowserRouter>
+   
     </>
   )
 }
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='text-center'>
+                Copyright Reserved at 2024-25 by Sameera Udesh
+            </p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
